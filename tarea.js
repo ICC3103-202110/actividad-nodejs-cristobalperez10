0282b0261ac237bc82bcace4250e6ccd5cd2eb6e
Cristóbal Pérez <caperez10@miuandes.cl>
2021-05-03 00:00:05 -0400

    Final

--- tarea.js
+++ tarea.js
@@ -1,1 +1,15 @@
-console.log("hola mundo")
+function Fibonacci(number) {
+    if(number < 2) return number;
+        return Fibonacci(number-1) + Fibonacci(number-2);
+}
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
+  
+  readline.question(`Enter a number:`, number => {
+    console.log(Fibonacci(number))
+    readline.close()
+    
+  })
